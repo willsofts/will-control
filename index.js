85484a48407f50f52772742de109e5e48895c50c
tassun <32536248+tassun@users.noreply.github.com>
2024-08-04 10:00:38 +0700

    add index

---
+++ index.js
@@ -0,0 +1,14 @@
+import DataPaging from "./controls/DataPaging.vue";
+import DataTable from "./controls/DataTable.vue";
+import InputDate from "./controls/InputDate.vue";
+import InputMask from "./controls/InputMask.vue";
+import InputMoney from "./controls/InputMoney.vue";
+import InputNumber from "./controls/InputNumber.vue";
+import InputTime from "./controls/InputTime.vue";
+import LoadingPage from "./controls/LoadingPage.vue";
+import PageHeader from "./controls/PageHeader.vue";
+
+export {
+    DataPaging, DataTable, InputDate, InputMask, InputMoney, 
+    InputNumber, InputTime, LoadingPage, PageHeader
+}
